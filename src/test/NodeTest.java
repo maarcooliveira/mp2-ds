5b1417ebd5cd23e9aeabb14685e42562a99628eb
Cassio Sousa <cassiosantossousa@gmail.com>
2015-04-18 19:39:43 -0500

    Created correct tests for NodeTest

--- src/test/NodeTest.java
+++ src/test/NodeTest.java
@@ -21,9 +21,23 @@ public class NodeTest {
 
     @Test
     public void testStoredKeys() {
-        assert testNode.storedKeys(20, 15, 32).equals("16 17 18 19 20");
-        assert testNode.storedKeys(5, 28, 32).equals("29 30 31 0 1 2 3 4 5");
-        assert testNode.storedKeys(5, 5, 8).equals("6 7 0 1 2 3 4 5");
+        assert testNode.storedKeys(12, 0, 32).equals("1 2 3 4 5 6 7 8 9 10 11 12");
+        assert testNode.storedKeys(20, 12, 32).equals("13 14 15 16 17 18 19 20");
         assert testNode.storedKeys(6, 5, 8).equals("6");
+        assert testNode.storedKeys(0, 20, 32).equals("0 21 22 23 24 25 26 27 28 29 30 31");
+        assert testNode.storedKeys(5, 5, 8).equals("0 1 2 3 4 5 6 7");
+        assert testNode.storedKeys(8, 12, 16).equals("0 1 2 3 4 5 6 7 8 13 14 15");
+    }
+
+    public String storedKeys(int nodeID, int prevID, int totalKeys) {
+        int firstKey = (prevID + 1) % totalKeys;
+        int lastKey = prevID < nodeID ? nodeID : nodeID + totalKeys;
+        ArrayList<Integer> allNumbers = new ArrayList<Integer>();
+        for (int key = firstKey; key <= lastKey; key++) {
+            Integer modularKey = key % totalKeys;
+            allNumbers.add(modularKey);
+        }
+        Collections.sort(allNumbers);
+        return allNumbers.toString().replace("[", "").replace("]", "").replace(",", "");
     }
 }
